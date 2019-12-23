3b1db1492c71c67e57c12e3991295162c568607a
leejunhwi <kajundrama@gmail.com>
2019-12-23 23:23:57 +0900

    jwt-security example 0.1

---
+++ src/main/java/com/kajundrama/jwt/KajundramaExampleJwtApplication.java
@@ -0,0 +1,13 @@
+package com.kajundrama.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KajundramaExampleJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KajundramaExampleJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kajundrama/jwt/KajundramaExampleJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kajundrama.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KajundramaExampleJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
